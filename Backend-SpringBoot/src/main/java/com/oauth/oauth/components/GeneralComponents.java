dc6ace93bfe3833ac8c5c9ef5dede09358ee2ca3
Harsh-Srivastav123 <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-03-10 23:58:14 +0530

    added redirect url

--- Backend-SpringBoot/src/main/java/com/oauth/oauth/components/GeneralComponents.java
+++ Backend-SpringBoot/src/main/java/com/oauth/oauth/components/GeneralComponents.java
@@ -2,11 +2,16 @@ package com.oauth.oauth.components;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GeneralComponents {
 
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
     }
+
+
+
 }
--- Backend-SpringBoot/src/main/java/com/oauth/oauth/configuration/SecurityConfigurations.java
+++ Backend-SpringBoot/src/main/java/com/oauth/oauth/configuration/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package com.oauth.oauth.configuration;
 
+import com.oauth.oauth.components.OAuth2LoginSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -12,19 +14,25 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations {
+    @Autowired
+    OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+
         http.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/addLanguageList","/addLanguage")
-                .permitAll()
+//                .requestMatchers("/addLanguageList","/addLanguage")
+//                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .successHandler(oAuth2LoginSuccessHandler);
+
+
 
         return http.build();
     }
