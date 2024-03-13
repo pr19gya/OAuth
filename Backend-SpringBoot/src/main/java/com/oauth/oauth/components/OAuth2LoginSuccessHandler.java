c29128efddb91fdf78970a52417e339210119a42
Harsh-Srivastav123 <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-03-13 18:59:34 +0530

    added logout feature

--- Backend-SpringBoot/src/main/java/com/oauth/oauth/components/OAuth2LoginSuccessHandler.java
+++ Backend-SpringBoot/src/main/java/com/oauth/oauth/components/OAuth2LoginSuccessHandler.java
@@ -3,6 +3,8 @@ package com.oauth.oauth.components;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,13 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 @Component
 public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Value("${homepageUrl}")
+    String homepageUrl;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         this.setAlwaysUseDefaultTargetUrl(true);
-        this.setDefaultTargetUrl("http://localhost:3000/");
+        this.setDefaultTargetUrl(homepageUrl);
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
--- Backend-SpringBoot/src/main/java/com/oauth/oauth/configuration/SecurityConfigurations.java
+++ Backend-SpringBoot/src/main/java/com/oauth/oauth/configuration/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.oauth.oauth.configuration;
 
 import com.oauth.oauth.components.OAuth2LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations {
+
+    @Value("${loginPageUrl}")
+    String loginPageUrl;
     @Autowired
     OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
@@ -28,7 +32,8 @@ public class SecurityConfigurations {
                 .authenticated()
                 .and()
                 .oauth2Login()
-                .successHandler(oAuth2LoginSuccessHandler);
+                .successHandler(oAuth2LoginSuccessHandler)
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl(loginPageUrl).permitAll();
 
 
         return http.build();
